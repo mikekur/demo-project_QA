078749c3052fbb516b9deb47386627da2a697d0f
mykhailo kurdiukov <mykhailo.kurdiukov@gmail.com>
2021-09-24 13:20:46 +0300

    changed sout in Main again

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello i'm master main!");
+        System.out.println("Hello i'm master main! " +
+                "and i'm changed again");
     }
 }
